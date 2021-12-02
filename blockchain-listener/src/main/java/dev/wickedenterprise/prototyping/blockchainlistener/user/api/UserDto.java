7c7b0ed1bff2396aa96c4693dc8a4f2b9fda2c41
Alexander C <alexander.culum@gmail.com>
2021-12-02 17:29:41 +0100

    Add all blockchain projects, yeah!

---
+++ blockchain-listener/src/main/java/dev/wickedenterprise/prototyping/blockchainlistener/user/api/UserDto.java
@@ -0,0 +1,11 @@
+package dev.wickedenterprise.prototyping.blockchainlistener.user.api;
+
+import java.util.List;
+
+@lombok.Data
+public class UserDto {
+
+    private String status;
+    private List<Data> data;
+
+}
